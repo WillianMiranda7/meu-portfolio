c334d43c97225b155228cbbbba440ec844c7eac4
Willian Miranda <157065847+WillianMiranda7@users.noreply.github.com>
2024-05-06 09:30:44 -0300

    menu improvements
    touching outside of menu to close it

--- meu-portfólio/JavaScript/script.js
+++ meu-portfólio/JavaScript/script.js
@@ -1,9 +1,11 @@
+const menuUl = document.getElementById("menuUl")
+const menuBt = document.getElementById("menuHamburger")
+const closeBt = document.getElementById("closeButton")
+
+menuBt.addEventListener('click', toOpenMenu)
+
 // To open menu button
 function toOpenMenu() {
-    let menuUl = document.getElementById("menuUl")
-    let menuBt = document.getElementById("menuHamburger")
-    let closeBt = document.getElementById("closeButton")
-    
     menuUl.style.display = "block"
     menuBt.style.display = "none"
     closeBt.style.display = "block"
@@ -11,11 +13,13 @@ function toOpenMenu() {
 
 // To close menu button
 function toCloseMenu() {
-    let menuUl = document.getElementById("menuUl")
-    let menuBt = document.getElementById("menuHamburger")
-    let closeBt = document.getElementById("closeButton")
-    
-    menuUl.style.display = "none"
-    menuBt.style.display = "block"
-    closeBt.style.display = "none"
+        menuUl.style.display = "none"
+        menuBt.style.display = "block"
+        closeBt.style.display = "none"
 }
+
+document.addEventListener('click', function (event){
+    if (event.target != menuBt && event.target != menuUl.contains && menuBt.style.display === "none"){
+        toCloseMenu()
+    }
+} )
