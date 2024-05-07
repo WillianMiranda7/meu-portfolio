c68af5ff8371ce19315743f726e8bad54addf174
Willian Miranda <157065847+WillianMiranda7@users.noreply.github.com>
2024-05-07 00:52:09 -0300

    07/05/2024 - menu animations

--- meu-portfólio/JavaScript/script.js
+++ meu-portfólio/JavaScript/script.js
@@ -1,25 +1,71 @@
 const menuUl = document.getElementById("menuUl")
 const menuBt = document.getElementById("menuHamburger")
-const closeBt = document.getElementById("closeButton")
 
-menuBt.addEventListener('click', toOpenMenu)
+menuBt.addEventListener('click', toggleMenu)
 
-// To open menu button
-function toOpenMenu() {
-    menuUl.style.display = "block"
-    menuBt.style.display = "none"
-    closeBt.style.display = "block"
+function toggleMenu() {
+    const isActive = menuBt.classList.contains('activate')
+    const isActiveUl = menuUl.classList.contains('activateUl')
+
+    menuBt.classList.toggle('activate', !isActive)
+    menuUl.classList.toggle('activateUl', !isActiveUl)
+
+    menuUl.className = isActive && isActiveUl ? '' : 'activateUl'
 }
 
-// To close menu button
-function toCloseMenu() {
-        menuUl.style.display = "none"
-        menuBt.style.display = "block"
-        closeBt.style.display = "none"
-}
 
-document.addEventListener('click', function (event){
-    if (event.target != menuBt && event.target != menuUl.contains && menuBt.style.display === "none"){
-        toCloseMenu()
-    }
-} )
+
+
+
+
+
+
+
+
+// const menuUl = document.getElementById("menuUl")
+// const menuBt = document.getElementById("menuHamburger")
+
+// menuBt.addEventListener('click', toggleMenu)
+
+// function toggleMenu() {
+//     const isActive = menuBt.classList.contains('activate')
+
+//     menuBt.classList.toggle('activate', !isActive)
+
+//     menuUl.style.display = isActive ? 'none' : 'block'
+// }
+
+
+
+
+
+
+
+
+// const menuUl = document.getElementById("menuUl")
+// const menuBt = document.getElementById("menuHamburger")
+// const closeBt = document.getElementById("closeButton")
+// const menuLines = document.querySelectorAll('.hamburgerLine')
+
+// menuBt.addEventListener('click', toOpenMenu)
+
+// // To open menu button
+// function toOpenMenu() {
+//     menuUl.style.display = "block"
+//     // menuBt.style.display = "none"
+//     // closeBt.style.display = "block"
+// }
+
+// // To close menu button
+// function toCloseMenu() {
+//     menuUl.style.display = "none"
+//     // menuBt.style.display = "block"
+//     // closeBt.style.display = "none"
+// }
+
+// document.addEventListener('click', function (event) {
+//     if (event.target != menuBt && event.target != menuUl.contains /*&& menuBt.style.display === "none"*/) {
+//         toCloseMenu()
+//     }
+// })
+
